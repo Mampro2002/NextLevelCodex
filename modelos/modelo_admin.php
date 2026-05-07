3a78758792b89cd315458e2b985394d77570d483
Manuel Acevedo Marin <6003424@viewnext.com>
2026-05-07 12:35:55 +0200

    Mejora en las actualizaciones de Imagen

--- modelos/modelo_admin.php
+++ modelos/modelo_admin.php
@@ -35,7 +35,17 @@ class ModeloAdmin
     public function actualizarJuego($id, $titulo, $desarrollador, $distribuidora, $fecha, $genero, $descripcion, $portada, $enlace_compra, $trailer, $en_desarrollo)
     {
         include "../sec/bdd.php";
+
+        // Si se proporciona una nueva portada, eliminar la anterior (si no es la default)
         if ($portada) {
+            $filt = $db->prepare("SELECT portada FROM juegos WHERE id = ?");
+            $filt->bind_param("i", $id);
+            $filt->execute();
+            $anterior = $filt->get_result()->fetch_assoc()['portada'];
+            if ($anterior && $anterior != 'default_game.jpg' && file_exists("../assets/img/games/" . $anterior)) {
+                unlink("../assets/img/games/" . $anterior);
+            }
+
             $filt = $db->prepare("UPDATE juegos SET titulo=?, desarrollador=?, distribuidora=?, fecha_lanzamiento=?, genero=?, descripcion=?, portada=?, enlace_compra=?, trailer=?, en_desarrollo=? WHERE id=?");
             $filt->bind_param("sssssssssii", $titulo, $desarrollador, $distribuidora, $fecha, $genero, $descripcion, $portada, $enlace_compra, $trailer, $en_desarrollo, $id);
         } else {
@@ -103,7 +113,17 @@ class ModeloAdmin
     public function actualizarElemento($id, $nombre, $tipo, $valor1, $valor2, $rareza, $descripcion, $imagen)
     {
         include "../sec/bdd.php";
+
+        // Si se proporciona una nueva imagen, eliminar la anterior
         if ($imagen) {
+            $filt = $db->prepare("SELECT imagen FROM elementos WHERE id = ?");
+            $filt->bind_param("i", $id);
+            $filt->execute();
+            $anterior = $filt->get_result()->fetch_assoc()['imagen'];
+            if ($anterior && file_exists("../assets/img/elementos/" . $anterior)) {
+                unlink("../assets/img/elementos/" . $anterior);
+            }
+
             $filt = $db->prepare("UPDATE elementos SET nombre=?, tipo=?, valor1=?, valor2=?, rareza=?, descripcion=?, imagen=? WHERE id=?");
             $filt->bind_param("sssssssi", $nombre, $tipo, $valor1, $valor2, $rareza, $descripcion, $imagen, $id);
         } else {
@@ -159,7 +179,17 @@ class ModeloAdmin
     public function actualizarPersonaje($id, $nombre, $rol, $ubicacion, $descripcion, $imagen)
     {
         include "../sec/bdd.php";
+
+        // Si se proporciona una nueva imagen, eliminar la anterior
         if ($imagen) {
+            $filt = $db->prepare("SELECT imagen FROM personajes WHERE id = ?");
+            $filt->bind_param("i", $id);
+            $filt->execute();
+            $anterior = $filt->get_result()->fetch_assoc()['imagen'];
+            if ($anterior && file_exists("../assets/img/personajes/" . $anterior)) {
+                unlink("../assets/img/personajes/" . $anterior);
+            }
+
             $filt = $db->prepare("UPDATE personajes SET nombre=?, rol=?, ubicacion=?, descripcion=?, imagen=? WHERE id=?");
             $filt->bind_param("sssssi", $nombre, $rol, $ubicacion, $descripcion, $imagen, $id);
         } else {
@@ -189,18 +219,20 @@ class ModeloAdmin
     public function actualizarMapaImagen($id_juego, $imagen)
     {
         include "../sec/bdd.php";
+
+        // Obtener y eliminar la imagen de mapa anterior
+        $filt = $db->prepare("SELECT mapa_imagen FROM juegos WHERE id = ?");
+        $filt->bind_param("i", $id_juego);
+        $filt->execute();
+        $anterior = $filt->get_result()->fetch_assoc()['mapa_imagen'];
+        if ($anterior && file_exists("../assets/img/maps/" . $anterior)) {
+            unlink("../assets/img/maps/" . $anterior);
+        }
+
+        // Actualizar con la nueva imagen y activar el flag
         $filt = $db->prepare("UPDATE juegos SET mapa_imagen=?, tiene_mapa = 1 WHERE id=?");
         $filt->bind_param("si", $imagen, $id_juego);
         return $filt->execute();
-    }
-
-    public function obtenerPuntosMapa($id_juego)
-    {
-        include "../sec/bdd.php";
-        $filt = $db->prepare("SELECT * FROM mapas WHERE id_juego = ?");
-        $filt->bind_param("i", $id_juego);
-        $filt->execute();
-        return $filt->get_result()->fetch_all(MYSQLI_ASSOC);
     }
 
     public function insertarPuntoMapa($id_juego, $nombre, $pos_x, $pos_y, $tipo, $descripcion, $icono)
